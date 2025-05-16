2633f530e1b73b39b74e33bb2f189ea845575806
muhamadairul <akunpertamapublic@gmail.com>
2025-05-16 18:51:28 +0700

    init project

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+
+const NavbarComponent = () => {
+  return (
+    //posisikan navbar di tengah
+    <Navbar className="justify-content-center" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <strong>Rull</strong> App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
